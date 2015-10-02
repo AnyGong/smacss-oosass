3babeb49eb0d0dbc54579c5473805e4866edd644
gongxiaoming <550843110@qq.com>
2015-10-02 15:01:54 +0800

    修复名称命名

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@
  * Created by AnyGong on 2015/9/6.
  */
 var gulp = require('gulp');
-var sassdoc = require('sassdoc');
+//var sassdoc = require('sassdoc');
 var sassdocSetting=require('./package.json').sassdoc;
 var sass = require('gulp-ruby-sass');
-gulp.task('sassdoc', function () {
-    return gulp.src(sassdocSetting.source)
-        .pipe(sassdoc(sassdocSetting));
-});
+//gulp.task('sassdoc', function () {
+//    return gulp.src(sassdocSetting.source)
+//        .pipe(sassdoc(sassdocSetting));
+//});
 
 gulp.task('sass', function () {
-    return sass('sass/style.scss', {noCache: true,style:'compressed'})
+    return sass('sass/style.scss', {noCache: true,style:'expand'})
         .on('error', sass.logError)
-        .pipe(gulp.dest('sass/'))
+        .pipe(gulp.dest('test/'))
 });
 gulp.task('watch', function () {
     gulp.watch(pkg.source, ['sassdoc']);
